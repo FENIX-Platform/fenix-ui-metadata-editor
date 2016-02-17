731093a39c94e6e91f8682ca4565003c61d95ff8
Daniele Salvatore <daniele.salvatore@fao.org>
2016-02-17 12:07:41 +0100

    Use requireJs aliases for configuration

--- config/schemas/identification.js
+++ config/schemas/identification.js
@@ -63,7 +63,7 @@
             "language": {
                 "helper": "Language used by the resource for textual information.",
                 "type": "select",
-                "dataSource": "./submodules/fenix-ui-metadata-editor/config/CL/CL_Languages.json"
+                "dataSource": "fx-MetaEditor2/config/CL/CL_Languages.json"
             },
             "languageDetails": {
                 "helper": "Comments and additional details about the language used for the textual information of the resource. This field is addressed to highlight some particular inconsistencies in the language (or languages) used in the resource, if any. For example to alert that the resource is not completely homogeneous in the language used for textual information. Otherwise it can be leaved empty.",
@@ -71,7 +71,7 @@
             "characterSet": {
                 "helper": "Full name of the character coding standard used by the resource.",
                 "type": "select",
-                "dataSource": "./submodules/fenix-ui-metadata-editor/config/CL/CL_IANACharacterSet.json"
+                "dataSource": "fx-MetaEditor2/config/CL/CL_IANACharacterSet.json"
             },
             "metadataStandardName": {
                 "helper": "Name of the metadata standard specifications used. In FENIX framework this field would be pre-compiled by \u0027FENIX\u0027.",
@@ -82,7 +82,7 @@
             "metadataLanguage": {
                 "helper": "Version of the metadata standard specifications used.",
                 "type": "select",
-                "dataSource": "./submodules/fenix-ui-metadata-editor/config/CL/CL_Languages.json"
+                "dataSource": "fx-MetaEditor2/config/CL/CL_Languages.json"
             },
             "noDataValue": {
                 "helper": "Value assigned to the cells to represent the absence of data. Missing values are usually highlight through apposite ags, however the data matrix does not report empty cells but a predefined combination of characters (such as \u0027NA\u0027, \u0027000\u0027 . . . ) indicating the absence of data."
